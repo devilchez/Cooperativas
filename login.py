318ec6f63fa441ed2d1ffcc667aac0bd31c3f58d
devilchez <vilchezruizdelia@gmail.com>
2025-07-14 16:54:11 -0600

    Update login.py

--- login.py
+++ login.py
@@ -1,1 +1,22 @@
+from config.conexion import obtener_conexion
+
+def verificar_credenciales(id_usuario, contrasena):
+
+    try:
+        conexion = obtener_conexion()
+        cursor = conexion.cursor()
+        consulta = "SELECT Nombre FROM Empleado WHERE Id_empleado = %s AND Contrasena = %s"
+        cursor.execute(consulta, (id_usuario, contrasena))
+        resultado = cursor.fetchone()
+        cursor.close()
+        conexion.close()
+
+        if resultado:
+            return resultado[0]  #
+        else:
+            return None
+
+    except Exception as e:
+        print(f"Error en verificar_credenciales: {e}")
+        return None
 
