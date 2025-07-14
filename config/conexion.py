45b6e2750506a96df77f12fbb51f68942df3e6c8
devilchez <vilchezruizdelia@gmail.com>
2025-07-14 16:44:08 -0600

    Update conexion.py

--- config/conexion.py
+++ config/conexion.py
@@ -1,1 +1,21 @@
+import mysql.connector
+from mysql.connector import Error
 
+def obtener_conexion():
+    try:
+        conexion = mysql.connector.connect(
+            host='bub3mpe7frqgcgokkhwexm-mysql.services.clever-cloud.com',
+            user='uaxfwo75yaciiqsf',
+            password='DTzflvjMCo4FjFfuuvME',
+            database='b3mpe7frqgcgokkhwexm',
+            port=3306
+        )
+        if conexion.is_connected():
+            print("✅ Conexión establecida")
+            return conexion
+        else:
+            print("❌ Conexión fallida (is_connected = False)")
+            return None
+    except mysql.connector.Error as e:
+        print(f"❌ Error al conectar: {e}")
+        return None
