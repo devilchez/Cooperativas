89bdb926dd5fea4f15051eee290725588b3ba063
devilchez <vilchezruizdelia@gmail.com>
2025-07-14 18:02:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,26 +7,6 @@ for key in ['logged_in', 'usuario', 'nombre_usuario', 'module']:
     if key not in st.session_state:
         st.session_state[key] = None
 
-def login():
-    st.title("🔐 Ingreso al Sistema")
-    st.write("Ingresa tu ID de empleado y contraseña.")
-
-    id_usuario = st.text_input("ID de Usuario", placeholder="Ejemplo: MAR01", key="usuario_input")
-    contrasena = st.text_input("Contraseña", type="password", key="contrasena_input")
-
-    if st.button("Iniciar sesión"):
-        resultado = verificar_usuario(id_usuario, contrasena)
-        if resultado:
-            nombre, nivel_usuario = resultado
-            st.session_state.logged_in = True
-            st.session_state.usuario = id_usuario.strip()
-            st.session_state.nombre_usuario = nombre
-            st.session_state.nivel_usuario = nivel_usuario
-            st.success(f"✅ Bienvenida {nombre} ({nivel_usuario})")
-            st.rerun()
-        else:
-            st.error("❌ ID o contraseña incorrectos")
-
 
 def menu_principal():
     st.title("🏠 Menú Principal")
