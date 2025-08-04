cb9a2814a61ca7c814e1996349ca59875285d0c6
Alexis-Marti <00065821@uca.edu.sv>
2025-08-04 16:07:16 -0600

    Update ventas.py

--- modulos/ventas.py
+++ modulos/ventas.py
@@ -92,9 +92,10 @@ def modulo_ventas():
                     producto_venta = {
                         "cod_barra": cod_barras_input,
                         "nombre": nombre_producto,
-                        "precio_venta": precio_venta,
+                        "precio_unitario": precio_venta,
                         "cantidad": cantidad_libras if cantidad_libras is not None else cantidad,
-                        "subtotal": subtotal
+                        "subtotal": subtotal,
+                        "tipo_cliente": tipo_cliente
                     }
                     st.session_state["productos_vendidos"].append(producto_venta)
                     st.session_state["limpiar_cod"] = True
@@ -111,7 +112,7 @@ def modulo_ventas():
         for i, prod in enumerate(st.session_state["productos_vendidos"]):
             st.markdown(
                 f"**{prod['nombre']}** — {prod['cantidad']} unidad(es) — "
-                f"Precio: ${prod['precio_venta']:.2f} — Subtotal: ${prod['subtotal']:.2f}"
+                f"Precio: ${prod['precio_unitario']:.2f} — Subtotal: ${prod['subtotal']:.2f} — Tipo de cliente: {prod['tipo_cliente']}"
             )
             total_venta += prod["subtotal"]
 
@@ -135,13 +136,15 @@ def modulo_ventas():
 
                 for prod in st.session_state["productos_vendidos"]:
                     cursor.execute("""
-                        INSERT INTO ProductoxVenta (Id_venta, Cod_barra, Cantidad_vendida, Precio_unitario)
-                        VALUES (%s, %s, %s, %s)
+                        INSERT INTO ProductoxVenta (
+                            Id_venta, Cod_barra, Cantidad_vendida, Tipo_de_cliente, Precio_Unitario
+                        ) VALUES (%s, %s, %s, %s, %s)
                     """, (
                         nuevo_id_venta,
                         prod["cod_barra"],
                         prod["cantidad"],
-                        prod["precio_venta"]
+                        prod["tipo_cliente"],
+                        prod["precio_unitario"]
                     ))
 
                 conn.commit()
